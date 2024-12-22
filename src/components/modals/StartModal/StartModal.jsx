218dcd55e68e872d3c36b78ca10f0fe7eb7a5b87
DIEGUCHIN\ruizd <ruizdiego.-@hotmail.com>
2024-12-22 19:06:25 +0100

    startModal, changes

--- src/components/modals/StartModal/StartModal.jsx
+++ src/components/modals/StartModal/StartModal.jsx
@@ -32,60 +32,60 @@ const StartModal = () => {
         >
             {/* <div className={styles.Marco}></div> */}
 
-                <div className={styles.header}>
-                    <div className={styles.titleContainer}>
-                        <h2 className={styles.title}>{t('help')}</h2>
+            <div className={styles.header}>
+                <div className={styles.titleContainer}>
+                    <h2 className={styles.title}>{t('help')}</h2>
+                </div>
+                <div className={styles.langContainer}>
+                    <div
+                        className={styles.lang}
+                        style={{ transform: selectedLanguage === 'Esp' ? 'scale(0.8)' : 'scale(1)' }}
+                        onClick={() => {
+                            handleLanguageClick('Esp');
+                        }}
+                    >
+                        <img src={Esp} alt="Bandera Española" className={styles.flag} />
                     </div>
-                    <div className={styles.langContainer}>
-                        <div
-                            className={styles.lang}
-                            style={{ transform: selectedLanguage === 'Esp' ? 'scale(0.8)' : 'scale(1)' }}
-                            onClick={() => {
-                                handleLanguageClick('Esp');
-                            }}
-                        >
-                            <img src={Esp} alt="Bandera Española" className={styles.flag} />
-                        </div>
 
-                        <div
-                            className={styles.lang}
-                            style={{ transform: selectedLanguage === 'UK' ? 'scale(0.8)' : 'scale(1)' }}
-                            onClick={() => {
-                                handleLanguageClick('UK');
-                            }}
-                        >
-                            <img src={Eng} alt="Bandera inglesa" className={styles.flag} />
-                        </div>
+                    <div
+                        className={styles.lang}
+                        style={{ transform: selectedLanguage === 'UK' ? 'scale(0.8)' : 'scale(1)' }}
+                        onClick={() => {
+                            handleLanguageClick('UK');
+                        }}
+                    >
+                        <img src={Eng} alt="Bandera inglesa" className={styles.flag} />
+                    </div>
 
-                        <div
-                            className={styles.lang}
-                            style={{ transform: selectedLanguage === 'Cat' ? 'scale(0.8)' : 'scale(1)' }}
-                            onClick={() => {
-                                handleLanguageClick('Cat');
-                            }}
-                        >
-                            <img src={Cat} alt="Bandera catalana" className={styles.flag} />
-                        </div>
+                    <div
+                        className={styles.lang}
+                        style={{ transform: selectedLanguage === 'Cat' ? 'scale(0.8)' : 'scale(1)' }}
+                        onClick={() => {
+                            handleLanguageClick('Cat');
+                        }}
+                    >
+                        <img src={Cat} alt="Bandera catalana" className={styles.flag} />
                     </div>
                 </div>
+            </div>
 
-                <div className={styles.modalContent}>
-                    <p className={styles.row}>{t('startModal1')}</p>
-                    <p className={styles.row}>{t('startModal2-1')} <span style={{ color: 'yellow' }}>mobile </span> {t('startModal2-2')}
-                        <img src={GetIn} alt="" className={styles.icon} /> {t('startModal2-3')}</p>
-                    <div className={styles.imageDescription}>
-                        <img src={Swipper} alt="" className={styles.icon} /><p>{t('startModal3')}</p>
-                    </div>
-                    <div className={styles.imageDescription}>
-                        <img src={AccessControl} alt="" className={styles.icon} /><p>{t('startModal4')}</p>
-                    </div>
-                    <p className={styles.row}>{t('startModal2-1')} <span style={{ color: 'yellow' }}>web </span>{t('startModal5')}</p>
-
-                    <p className={styles.row}>{t('startModal6')}</p>
-
+            <div className={styles.modalContent}>
+                <p className={styles.row}>{t('startModal1')}</p>
+                <p className={styles.row}>{t('startModal2-1')} <span style={{ color: 'yellow' }}>mobile </span> {t('startModal2-2')}
+                    <img src={GetIn} alt="" className={styles.icon} /> {t('startModal2-3')}</p>
+                <div className={styles.imageDescription}>
+                    <img src={Swipper} alt="" className={styles.icon} /><p>{t('startModal3')}</p>
                 </div>
+                <div className={styles.imageDescription}>
+                    <img src={AccessControl} alt="" className={styles.icon} /><p>{t('startModal4')}</p>
+                </div>
+                <p className={styles.row}>{t('startModal2-1')} <span style={{ color: 'yellow' }}>web </span>{t('startModal5')}</p>
+
+                <p className={styles.row}>{t('startModal6')}</p>
+
+
                 <div className={styles.button} onClick={closeModal}>{t('close')}</div>
-
+            </div>
         </Modal >
     );
 };
