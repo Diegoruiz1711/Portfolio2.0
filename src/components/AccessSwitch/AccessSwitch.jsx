70edaaf08c9c1f26e80ecf7114416902cd3a8c57
DIEGUCHIN\ruizd <ruizdiego.-@hotmail.com>
2024-12-22 18:00:56 +0100

    prueba del switch: eliminacion de imagenes

--- src/components/AccessSwitch/AccessSwitch.jsx
+++ src/components/AccessSwitch/AccessSwitch.jsx
@@ -1,29 +1,50 @@
-import styles from './AccessSwitch.module.css'
+// import styles from './AccessSwitch.module.css'
+// import { WebContext } from '../../WebContext/WebContext';
+// import { useContext } from 'react';
+// import RedLight from '../..//assets/images/redLight.png'
+// import GreenLight from '../..//assets/images/greenLight.png'
+
+// const AccessSwitch = () => {
+
+//     const { accessLight,handleAccessLight } = useContext(WebContext);
+
+//     return (
+
+//         <div className={styles.container} >
+//             <label className={styles.switch}>
+//                 <input type="checkbox" checked={accessLight} onChange={handleAccessLight} />
+//                 <span className={styles.slider}>
+//                     {accessLight ? (
+//                         <img src={GreenLight} alt="Red Light" className={styles.light} />
+//                     ) : (
+//                         <img src={RedLight} className={styles.light} alt="Green Light" />
+//                     )}
+//                 </span>
+//             </label>
+//         </div>
+
+//     )
+// }
+
+// export default AccessSwitch;
+
+import styles from './AccessSwitch.module.css';
 import { WebContext } from '../../WebContext/WebContext';
 import { useContext } from 'react';
-import RedLight from '../..//assets/images/redLight.png'
-import GreenLight from '../..//assets/images/greenLight.png'
 
 const AccessSwitch = () => {
-
-    const { accessLight,handleAccessLight } = useContext(WebContext);
+    const { accessLight, handleAccessLight } = useContext(WebContext);
 
     return (
-
-        <div className={styles.container} >
+        <div className={styles.container}>
             <label className={styles.switch}>
                 <input type="checkbox" checked={accessLight} onChange={handleAccessLight} />
                 <span className={styles.slider}>
-                    {accessLight ? (
-                        <img src={GreenLight} alt="Red Light" className={styles.light} />
-                    ) : (
-                        <img src={RedLight} className={styles.light} alt="Green Light" />
-                    )}
+                    <span className={`${styles.light} ${accessLight ? styles.green : styles.red}`}></span>
                 </span>
             </label>
         </div>
-
-    )
-}
+    );
+};
 
 export default AccessSwitch;
